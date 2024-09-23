dbc965571908bf09b23b9ef0903ee622881d5505
Fatih Kuzu <337920@student.mboutrecht.nl>
2024-09-23 11:56:52 +0200

    Les 9 is klaar. huiswerk gekregen voor les 10

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,34 +1,12 @@
 <?php
-namespace app\Models;
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model; // Make sure to import the Model class
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-    return [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$40000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40000'
-        ]
-    ];
-    }
-    public static function find(int $id): array 
-    {
-        $job =  Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (! $job) {
-            abort(404);
-        }
-    }
-        
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+}
