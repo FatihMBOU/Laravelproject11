bc61f388919d34ce4f439487786c343adcd876ca
Fatih Kuzu <337920@student.mboutrecht.nl>
2024-10-18 13:29:40 +0200

    Les 14 is klaar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::preventLazyLoading();
+
+    //    Paginator::useBootstrap();
+
     }
 }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -17,5 +17,9 @@
         </a>
     
     @endforeach
+
+    <div>
+        {{ $jobs->links() }}
+    </div>
     </ul>   
 </x-layout>
